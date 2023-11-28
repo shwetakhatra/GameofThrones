c28a7001c64879f87cad187bd77694328a3c9443
Shweta Khatra <khatrashweta10314@gmail.com>
2023-11-28 10:29:58 +0100

    Loader changes in detailview

--- src/views/HouseDetails.vue
+++ src/views/HouseDetails.vue
@@ -1,22 +1,29 @@
 <template>
     <div class="container" style="padding: 60px 15px 0;">
-      <h3 class="mt-4" v-if="house[0]">{{ house[0].name }}</h3>
-      <div class="d-flex justify-content-end">
-        <router-link to="/houses" class="btn btn-danger text-white">
-          Back to houses
-        </router-link>
-    </div>
-      <div class="card mt-5" style="width: 100%;" v-if="house[0]">
-        <div class="card-body">
-          <div class="form-group mt-3">
-            <strong><label>Slug</label></strong>
-            <input type="text" class="form-control mt-2" v-model="house[0].slug" disabled>
-          </div>
-          <div class="form-group mt-3">
-            <strong><label class="font-bold">Members</label></strong>
-            <template v-for="member in house[0].members" :key="member.name">
-              <input type="text" class="form-control mt-2" v-model="member.name" disabled>
-            </template>
+      <div v-if="loading" class="loader-overlay">
+        <div class="spinner-border text-danger" role="status">
+          <span class="sr-only"></span>
+        </div>
+      </div>
+      <div v-else>
+        <h3 class="mt-4" v-if="house[0]">{{ house[0].name }}</h3>
+        <div class="d-flex justify-content-end">
+          <router-link to="/houses" class="btn btn-danger text-white">
+            Back to houses
+          </router-link>
+        </div>
+        <div class="card mt-5" style="width: 100%;" v-if="house[0]">
+          <div class="card-body">
+            <div class="form-group mt-3">
+              <strong><label>Slug</label></strong>
+              <input type="text" class="form-control mt-2" v-model="house[0].slug" disabled>
+            </div>
+            <div class="form-group mt-3">
+              <strong><label class="font-bold">Members</label></strong>
+              <template v-for="member in house[0].members" :key="member.name">
+                <input type="text" class="form-control mt-2" v-model="member.name" disabled>
+              </template>
+            </div>
           </div>
         </div>
       </div>
@@ -37,15 +44,19 @@
     data() {
       return {
         house: [] as House[],
+        loading: false,
       };
     },
     async mounted() {
       const slug = this.$route.params.slug;
       try {
+        this.loading = true; 
         const response = await (this as any).$axios.get(`/api/v1/house/${slug}`);
         this.house = response.data || {};
       } catch (error) {
         console.error('Error fetching house details:', error);
+      } finally {
+        this.loading = false;
       }
     },
   });
--- src/views/PersonDetails.vue
+++ src/views/PersonDetails.vue
@@ -1,34 +1,41 @@
 <template>
     <div class="container" style="padding: 60px 15px 0;">
-        <h3 class="mt-4" v-if="personDetails[0]">{{ personDetails[0].name }}</h3>
-        <div class="d-flex justify-content-end">
-            <router-link to="/persons" class="btn btn-danger text-white">
-            Back to persons
-            </router-link>
+        <div v-if="loading" class="loader-overlay">
+            <div class="spinner-border text-danger" role="status">
+            <span class="sr-only"></span>
+            </div>
         </div>
-        <div class="card mt-5 mb-5" style="width: 100%;" v-if="personDetails[0]">
-            <div class="card-body">
-            <div class="form-group mt-3">
-                <strong><label>Slug</label></strong>
-                <input type="text" class="form-control mt-2" v-model="personDetails[0].slug" disabled>
+        <div v-else>
+            <h3 class="mt-4" v-if="personDetails[0]">{{ personDetails[0].name }}</h3>
+            <div class="d-flex justify-content-end">
+                <router-link to="/persons" class="btn btn-danger text-white">
+                Back to persons
+                </router-link>
             </div>
-            <div class="form-group mt-3">
-                <strong><label>House</label></strong>
-                <input type="text" class="form-control mt-2" v-model="personDetails[0].house.name" disabled>
-            </div>
-            <div class="form-group mt-3">
-                <strong><label class="font-bold">Quotes</label></strong>
-                <template v-for="quote in personDetails[0].quotes" :key="quote" v-if="newQuotes && newQuotes.length === 0">
-                    <p class="mt-2"><i>"{{ quote }}"</i></p>
-                </template>
-                <template v-else v-for="quote in newQuotes" :key="1">
-                    <p class="mt-2"><i>"{{ quote.sentence }}"</i></p>
-                </template>
-            </div>
-                <div class="d-flex justify-content-end">
-                    <button type="button" class="btn btn-danger" @click="replaceQuotes()">
-                        Replace quotes
-                    </button>
+            <div class="card mt-5 mb-5" style="width: 100%;" v-if="personDetails[0]">
+                <div class="card-body">
+                <div class="form-group mt-3">
+                    <strong><label>Slug</label></strong>
+                    <input type="text" class="form-control mt-2" v-model="personDetails[0].slug" disabled>
+                </div>
+                <div class="form-group mt-3">
+                    <strong><label>House</label></strong>
+                    <input type="text" class="form-control mt-2" v-model="personDetails[0].house.name" disabled>
+                </div>
+                <div class="form-group mt-3">
+                    <strong><label class="font-bold">Quotes</label></strong>
+                    <template v-for="quote in personDetails[0].quotes" :key="quote" v-if="newQuotes && newQuotes.length === 0">
+                        <p class="mt-2"><i>"{{ quote }}"</i></p>
+                    </template>
+                    <template v-else v-for="quote in newQuotes" :key="1">
+                        <p class="mt-2"><i>"{{ quote.sentence }}"</i></p>
+                    </template>
+                </div>
+                    <div class="d-flex justify-content-end">
+                        <button type="button" class="btn btn-danger" @click="replaceQuotes()">
+                            Replace quotes
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -51,8 +58,9 @@ export default defineComponent({
     name: 'HouseDetails',
     data() {
         return {
-        personDetails: []  as Person[],
-        newQuotes: [] as { sentence: string }[]
+            personDetails: []  as Person[],
+            newQuotes: [] as { sentence: string }[],
+            loading: false,
         };
     },
     async mounted() {
@@ -62,19 +70,25 @@ export default defineComponent({
         async fetchPersonDetails() {
             const slug = this.$route.params.slug;
             try {
+                this.loading = true;
                 const response = await (this as any).$axios.get(`/api/v1/character/${slug}`);
                 this.personDetails = response.data || {};
             } catch (error) {
                 console.error('Error fetching person details:', error);
+            } finally {
+                this.loading = false;
             }
         },
         async replaceQuotes(){
             const slug = this.$route.params.slug;
             try {
+                this.loading = true;
                 const response = await (this as any).$axios.get(`/api/v1/author/${slug}/5`);
                 this.newQuotes = response.data || {};
             } catch (error) {
                 console.error('Error fetching person details:', error);
+            } finally {
+                this.loading = false;
             }
         }
     }
